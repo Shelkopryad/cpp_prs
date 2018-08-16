15927b9e2120febf71a6cfd8ee1f0eba83b084ec
Свирин Денис Александрович <16734975@sigma.sbrf.ru>
2018-08-16 15:44:12 +0300

    wtf

---
+++ common/my_defines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define runner_method_name main
+#define open_bracket {
+#define close_bracket }
+#define print_in_console cout
+#define new_line endl
+#define returned_type int
+
+using namespace std;
+
+returned_type runner_method_name()
+open_bracket
+    print_in_console << "Asd" << new_line;
+    return 0;
+close_bracket
